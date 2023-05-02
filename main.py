4c0dfa9b538e531475fff20b7ec785a3fa17de2a
ThiagoSilva20 <thiago201714@gmail.com>
2023-05-01 23:35:35 -0300

    animation pygame

--- main.py
+++ main.py
@@ -17,34 +17,33 @@ class Personagem(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.sprites = []
     	
-        self.sprites.append(pygame.image.load('pixel_game/T_academia/sprite_0.png'))
+        self.sprites.append(pygame.image.load('T_academia/sprite_0.png'))
     	
-        self.sprites.append(pygame.image.load('pixel_game/T_academia/sprite_1.png'))
+        self.sprites.append(pygame.image.load('T_academia/sprite_1.png'))
     	
-        self.sprites.append(pygame.image.load('pixel_game/T_academia/sprite_2.png'))   
+        self.sprites.append(pygame.image.load('T_academia/sprite_2.png'))   
         
         self.atual = 0
         self.image = self.sprites[self.atual]
-        self.image = pygame.transform.scale(self.image, (32*6, 32*6))
+        self.image = pygame.transform.scale(self.image, (32*2, 32*2))
         self.rect = self.image.get_rect()
-        self.rect.topleft = 100, 100
-        self.animar = False
+        self.rect.topleft = 100, 220
+        
 
-    def atacar(self):
-        self.animar = True
     def update(self):
-        if self.animar == True:
             self.atual = self.atual + 0.5
             if self.atual >= len(self.sprites):
                 self.atual = 0
-                self.animar = False
             self.image = self.sprites[int(self.atual)]
-            self.image = pygame.transform.scale(self.image, (32*6, 32*6))
+            self.image = pygame.transform.scale(self.image, (32*2, 32*2))
 
 
 todas_as_sprites = pygame.sprite.Group()
 personagem = Personagem()
 todas_as_sprites.add(personagem)
+
+imagem_fundo = pygame.image.load('T_academia/back_image.jpg').convert()
+imagem_fundo = pygame.transform.scale(imagem_fundo, (640, 480))
 
 relogio = pygame.time.Clock()   
 while True:
@@ -54,8 +53,8 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             exit()
-        if event.type == KEYDOWN:
-            personagem.atacar()
+
+    tela.blit(imagem_fundo, (0,0))
     todas_as_sprites.draw(tela)
     todas_as_sprites.update()
     pygame.display.flip()
